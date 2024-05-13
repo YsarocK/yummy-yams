d6f6d4e36ed131b1fe1335453d5d5cefdfb37ca2
Pierre Keller <42378147+Nexk0@users.noreply.github.com>
2024-05-13 11:41:56 +0200

    Frontend and hot reloading working now

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+    ssr:false,
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
+    },
   modules:[
       "@pinia/nuxt"
   ]
